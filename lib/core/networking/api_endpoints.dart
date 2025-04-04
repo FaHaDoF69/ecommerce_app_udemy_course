c3dd0adc12df1f2f5a68daffd5b13703daebacf3
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2025-04-04 11:57:17 +0300

    Add api endpoints

--- lib/core/networking/api_endpoints.dart
+++ lib/core/networking/api_endpoints.dart
@@ -1,5 +1,18 @@
 class ApiEndpoints {
-  static const String baseUrl = "https://newsapi.org/v2/";
-  static const String searchEndpoint = "everything";
-  static const String topHeadlinesEndpoint = "top-headlines";
+  // Base URL for the API
+  static const String baseUrl = "https://fakestoreapi.com";
+
+  // Auth
+  static const String register = "/users";
+  static const String login = "/auth/login";
+
+  // Carts
+  static const String carts = "/carts";
+
+  // Products
+  static const String products = "/products";
+  static const String categories = "/products/categories";
+
+  // Category Products
+  static const String categoryProducts = "/category";
 }
